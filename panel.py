a75e19f589f8af15306f91f12836cdb53968b94b
cam <cameron.ryan.pears@gmail.com>
2024-11-14 02:31:09 +1300

    0.4.0 release

--- panel.py
+++ panel.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-dir_path = "./generated-pcbs/battery-dc-dc-converter-panel"
+dir_path = "./generated-pcbs/battery-dc-dc-panel"
 
 os.makedirs(dir_path, exist_ok=True)
 
@@ -15,8 +15,8 @@ command = [
     "--fiducials", "type: 4fid; hoffset: 3.85mm; voffset:6mm",
     "--tooling", "type: 4hole; hoffset: 3mm; voffset: 3mm; size: 2mm",
     "--copperfill", "type: solid; layers: all",
-    "./battery-dc-dc-converter/battery-dc-dc-converter.kicad_pcb",
-    f"{dir_path}/battery-dc-dc-converter-panel.kicad_pcb"
+    "./battery-dc-dc/battery-dc-dc.kicad_pcb",
+    f"{dir_path}/battery-dc-dc-panel.kicad_pcb"
 ]
 
 # Run the command
@@ -33,12 +33,12 @@ else:
 
 # replace '"lib_footprint_issues": "warning",'
 # with '"lib_footprint_issues": "ignore",'
-# in {dir_path}/battery-dc-dc-converter-panel.kicad_pro
+# in {dir_path}/battery-dc-dc-panel.kicad_pro
 
-with open(f"{dir_path}/battery-dc-dc-converter-panel.kicad_pro", "r") as file:
+with open(f"{dir_path}/battery-dc-dc-panel.kicad_pro", "r") as file:
     content = file.read()
 
 content = content.replace('"lib_footprint_issues": "warning",', '"lib_footprint_issues": "ignore",')
 
-with open(f"{dir_path}/battery-dc-dc-converter-panel.kicad_pro", "w") as file:
+with open(f"{dir_path}/battery-dc-dc-panel.kicad_pro", "w") as file:
     file.write(content)
